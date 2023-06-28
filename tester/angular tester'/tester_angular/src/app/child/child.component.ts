84ec6800ddd9d7ca8888719f2db9f920f3d393ed
temo1203 <temolortkipanidze2007@gmail.com>
2023-06-28 13:50:01 +0400

    angular things

--- tester/angular tester'/tester_angular/src/app/child/child.component.ts
+++ tester/angular tester'/tester_angular/src/app/child/child.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  SimpleChange,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
-export class ChildComponent {
+export class ChildComponent implements OnChanges {
   @Input() item = '';
+  @Input() item_2 = '';
   // @Output() newItemEvent = new EventEmitter<string>();
   // addNewItem(value: string) {
   //   this.newItemEvent.emit(value);
   //   console.log(value);
   // }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    setTimeout(() => {
+      if (this.item == 'parent_to_child') {
+        console.log('onchangetemo works');
+      }
+    }, 5000);
+  }
+  childToparent: string = 'child_To_parent';
   @Output() newItemEvent = new EventEmitter<any>();
   addNewItem(value: string) {
     this.newItemEvent.emit(value);
--- tester/angular tester'/tester_angular/src/app/parent/parent.component.ts
+++ tester/angular tester'/tester_angular/src/app/parent/parent.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  AfterViewInit,
+  OnInit,
+} from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
-export class ParentComponent {
+export class ParentComponent implements AfterViewInit, OnInit {
+  img: string = '../../assets/number-14-valentines-day-with-3d-hearts.jpg';
   currentItem = 'parent_to_child';
+  currentItem_2 = 'parent_to_child_2';
+  Item1: string = '';
+  lite1: string = '';
   items = ['item 1', 'item 2', 'item 3', 'item 4'];
   addItem2(thing: string) {
     this.items.push(thing);
@@ -14,4 +26,17 @@ export class ParentComponent {
   addItem(value: string) {
     this.items.push(value);
   }
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('ngoninit is called');
+    }, 5000);
+  }
+  @ViewChild(ChildComponent) child!: ChildComponent;
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.lite1 = this.child.item;
+      this.Item1 = this.child.childToparent;
+    }, 0);
+  }
+  bool: boolean = true;
 }
